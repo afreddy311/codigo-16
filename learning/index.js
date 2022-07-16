2a733917a959cc4ce8f290d58d7a67bba6a1f464
Freddy De La Cruz <afreddy.ds@gmail.com>
2022-07-15 23:17:00 -0500

    Charts.js

--- learning/index.js
+++ learning/index.js
@@ -32,28 +32,45 @@
 
 3  //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-let numeros=[];
-let sum=0;
-let sum1=0;
+// let numeros=[];
+// let sum=0;
+// let cont=0;
+// let prom = 0;
+// const resultado = document.querySelector(".resultado")
+// const resultado1 = document.querySelector(".resultado1")
 
-function Imprimirsuma() {
-    let numero = document.querySelector("#ingresar_numero");
-    let numero1 = numero.value;
+// function Imprimirsuma() {
+//     let numero = document.querySelector("#ingresar_numero");
+//     let numero1 = numero.value;
+
    
-    if (isNaN(numero1)){
-        alert('ingrese un numero correctamente');
-        
-    
-    } else if (!isNaN(numero1)) {
-        numeros.push=numero1;
-        for (let x of numeros){
-            sum1 = sum1+sum
-        }
-    } else  (numero1<0) 
-        document.write(numeros);
-        return;
+//     if (isNaN(numero1)){
+//         alert('ingrese un numero correctamente');
+//     }       
+//     if (!isNaN(numero1)) {
+//         numeros.push(numero1); 
+//          sum += Number(numero1) ; 
+//          cont +=1
+//          prom = sum/cont ;  
+//          console.log(numeros)
+//          console.log(sum)
+//          console.log(prom)
+//     }   
+//     if  (numero1<0) {
+//         resultado.innerHTML="<p> La suma de los numeros ingresados es " + sum + "</p>" ;
+//         resultado1.innerHTML="<p> El promedio de los numeros ingresados es " + prom + "</p>" ;
+//     }
+//     }   
+
+4 //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+
+
+
        
-}
+
 
 
 
